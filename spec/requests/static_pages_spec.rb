92fa52b6a1dd000b43603a8a8dadc114f8f74ecf
elena <elena.borovik@gameloft.com>
2014-01-31 15:55:05 +0200

    Added support folder for spec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,46 +3,30 @@ require 'spec_helper'
 describe "Static pages" do
 
   let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+  subject { page }
 
   describe "Home page" do
     before { visit root_path }
 
-    it "should have the content 'Sample App'" do
-      expect(page).to have_content('Sample App')
-    end
-
-    it "should have the title base title" do
-      expect(page).to have_title("#{base_title}")
-    end
-
-    it "should not have custom page title" do
-      expect(page).not_to have_title(" | Home")
-    end
+    it {should have_content('Sample App'))}
+    it {should have_title("#{base_title}")}
+    it {should_not have_title(" | Home")}
 
   end
 
   describe "Help page" do
     before { visit help_path }
 
-    it "should have the content 'Help'" do
-      expect(page).to have_content('Help')
-    end
-
-    it "should have the title 'Help'" do
-      expect(page).to have_title("#{base_title} | Help")
-    end
+    it {should have_content('Help')}
+    it {should have_title("#{base_title} | Help")}
 
   end
 
   describe "About page" do
     before { visit about_path }
 
-    it "should have the content 'About us'" do
-      expect(page).to have_content('About us')
-    end
+    it {should have_content('About us')}
+    it {should have_title("#{base_title} | About us")}
 
-    it "should have the title 'About us'" do
-      expect(page).to have_title("#{base_title} | About us")
-    end
   end
 end
