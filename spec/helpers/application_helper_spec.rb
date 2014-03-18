e9d3e3b99cb369554f36166d3d85cf874cfa1907
elena <elena.borovik@gameloft.com>
2014-03-18 13:45:26 +0200

    end charpter 8

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -15,13 +15,4 @@ describe ApplicationHelper do
       expect(full_title("")).not_to match(/\|/)
     end
   end
-
-
-
-  RSpec::Matchers.define :have_error_message do
-  match do |page|
-    expect(page).to have_selector('div.alert.alert-error')
-  end
-end
-
 end
